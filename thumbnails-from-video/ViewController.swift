604468dd076746a730894fa2b2430729400dc2a5
Ivan Lé Hjelmeland <ivan.hjelmeland@shortcut.no>
2021-11-30 13:44:47 +0100

    Created new Xcode project.

---
+++ thumbnails-from-video/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thumbnails-from-video
+//
+//  Created by Ivan Lé Hjelmeland on 30/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
